b4c8d9bdbe2e0b6b53a4b583c370b05fe63df5c3
ssuukko <tlstpstpdl11@gmail.com>
2024-06-11 22:19:54 +0900

    chapter 02 CODE 보충

--- src/신석호/chapter2/alice/Main.java
+++ src/신석호/chapter2/alice/Main.java
@@ -3,11 +3,16 @@ package 신석호.chapter2.alice;
 public class Main {
     public static void main(String[] args) {
         Alice alice = new Alice(140, "복도");
+        Alice alice2 = new Alice(140, "복도");
+
 
         Door door = new Door();
         Beverage beverage = new Beverage();
 
         alice.drinkBeverage(beverage);
         alice.passDoor(door);
+
+//        System.out.println(alice);
+//        System.out.println(alice2);
     }
 }
