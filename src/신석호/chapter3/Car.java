52b25fe9d9e0699e3e8d13c9a3dd4036af688f1f
ssuukko <tlstpstpdl11@gmail.com>
2024-06-12 15:51:02 +0900

    chapter 03 수정

---
+++ src/신석호/chapter3/Car.java
@@ -0,0 +1,11 @@
+package 신석호.chapter3;
+
+public abstract class Car {
+    public  void StartEngine(){
+        System.out.println("시동을 킵니다.");
+    };
+
+    public  void run(){
+        System.out.println("자동차가 앞으로 움직입니다.");
+    }
+}
---
+++ src/신석호/chapter3/SuperCar.java
@@ -0,0 +1,17 @@
+package 신석호.chapter3;
+
+public class SuperCar extends Car{
+    @Override
+    public void StartEngine() {
+        super.StartEngine();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public void fastRun(){
+        System.out.println("자동차가 매우 빠른 속도로 앞으로 갑니다.");
+    }
+}
