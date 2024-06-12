c3f2f93f0b8641466a8ae7d26f0fe3a51214509c
tjfwltn <tjfwltn1234@gmail.com>
2024-06-12 14:35:22 +0900

    chapter 3 완

---
+++ src/설지수/chapter3/Trump.java
@@ -0,0 +1,11 @@
+package 설지수.chapter3;
+
+public abstract class Trump {
+    void 납작엎드리다() {
+        System.out.println("납작 엎드리기");
+    }
+    void 뒤집어지다(){
+        System.out.println("뒤집어지기");
+    }
+
+}
---
+++ src/설지수/chapter3/TrumpHuman.java
@@ -0,0 +1,18 @@
+package 설지수.chapter3;
+
+public class TrumpHuman extends Trump{
+
+    @Override
+    void 납작엎드리다() {
+        super.납작엎드리다();
+    }
+
+    @Override
+    void 뒤집어지다() {
+        super.뒤집어지다();
+    }
+
+    void 걷다(){
+        System.out.println("걸을 때 몸이 종이처럼 좌우로 펄럭인다.");
+    }
+}
