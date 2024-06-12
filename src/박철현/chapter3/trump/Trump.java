9dc451d68561d40bf0598d8f9257f1b5f15c29bf
박철현 <tghy4@naver.com>
2024-06-12 13:10:17 +0900

    타입과 추상화 완료

---
+++ src/박철현/chapter3/trump/Trump.java
@@ -0,0 +1,10 @@
+package 박철현.chapter3.trump;
+
+public abstract class Trump {
+    public void 납작_업드리기() {
+        System.out.println("납잡 업드린다.");
+    }
+    public void 뒤집어지기() {
+        System.out.println("뒤집어진다.");
+    }
+}
---
+++ src/박철현/chapter3/trump/TrumpHuman.java
@@ -0,0 +1,16 @@
+package 박철현.chapter3.trump;
+
+public class TrumpHuman extends Trump{
+    @Override
+    public void 납작_업드리기() {
+        super.납작_업드리기();
+    }
+
+    @Override
+    public void 뒤집어지기() {
+        super.뒤집어지기();
+    }
+    public void 걷기() {
+        System.out.println("몸이 펄럭인다.");
+    }
+}
